640d7ebc3261237e103b226f046e439b6dbe01c9
nh3 <nh3@users.noreply.github.com>
2017-12-06 17:53:13 +0000

    @mod cMapInsulation.py: use scipy.interpolate.splev() to calculate derivates

--- cMapInsulation.py
+++ cMapInsulation.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 '''
-Usage: cMapInsulation.py [options] <input>
+Usage: cMapInsulation.py -r <res> -o <prfx> [options] <input>
 
 Options:
     -r <res>    resolution in bp
-    -d <minD>   minimum distance from the diagonal in bp [default: 0]
+    -o <prfx>   output prefix
     -s <size>   size of insulation block in bp [default: 100000]
-    -w <width>  width for smoothing by running mean in bp [default: 5000]
+    -d <minD>   minimum distance from the diagonal in bp [default: 0]
+    -w <width>  width for smoothing by running mean in number of bins [default: 11]
+    -z <minS>   minimum insulation domain size in number of bins [default: 5]
+    -v <minV>   minimum depth from both sides of the local minimums [default: 0.5]
     <input>     input sparse matrix
 '''
 
@@ -19,85 +22,193 @@ signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 import numpy as np
 import scipy.sparse
 import pandas as pd
+from scipy.interpolate import splrep,splev
 
-"""
-Eight-order approximation centered at grid point derived from:
-    Fornberg, Bengt (1988), "Generation of Finite Difference Formulas on Arbitrarily Spaced Grids",
-        Mathematics of Computation 51 (184): 699-706, doi:10.1090/S0025-5718-1988-0935077-0
-"""
-def prepare_derivative_kernel(n):
-    kernels = np.array([
-        [-1.0/280, -4.0/105,  1.0/5, -4.0/5,         0, 4.0/5, -1.0/5, 4.0/105, -1.0/280],
-        [-1.0/560,  8.0/315, -1.0/5,  8.0/5, -205.0/72, 8.0/5, -1.0/5, 8.0/315, -1.0/560]])
-    return kernels[n-1]
-
-
-def prepare_smooth_kernel(width, repeats=1):
-    rolling_mean_kernel = np.ones(width)/float(width)
-    kernel = rolling_mean_kernel
-    for i in range(repeats):
-        kernel = np.convolve(kernel, rolling_mean_kernel, mode='same')
-    return kernel
-
-
-#def prepare_derivative_kernel(n, width, smooth_times=1):
+#"""
+#Eight-order approximation centered at grid point derived from:
+#    Fornberg, Bengt (1988), "Generation of Finite Difference Formulas on Arbitrarily Spaced Grids",
+#        Mathematics of Computation 51 (184): 699-706, doi:10.1090/S0025-5718-1988-0935077-0
+#"""
+#def prepare_derivative_kernel(n):
 #    kernels = np.array([
 #        [-1.0/280, -4.0/105,  1.0/5, -4.0/5,         0, 4.0/5, -1.0/5, 4.0/105, -1.0/280],
 #        [-1.0/560,  8.0/315, -1.0/5,  8.0/5, -205.0/72, 8.0/5, -1.0/5, 8.0/315, -1.0/560]])
-#    rolling_mean_kernel = np.ones(width)/float(width)
-#    for i in range(smooth_times):
-#        kernel = np.convolve(kernels[n-1], rolling_mean_kernel)
-#    return 1e6*kernel[::-1]
+#    return kernels[n-1]
 
 
-def find_concave_regions(y, kernel, chrom='.', tol=1e-10):
-    d2y = np.convolve(y, kernel, mode='same')                                                                                                                                   
-    s = np.where(np.diff((d2y < tol).astype(int))==1)[0] + 1
-    e = np.where(np.diff((d2y < tol).astype(int))==-1)[0] + 1
-    if s[0] > e[0]:
-        s = np.insert(s, 0, 0)
-    if s[-1] > e[-1]:
-        e = np.insert(e, len(e), len(y))
-    v = [-np.mean(d2y[i:j]) for i,j in zip(s,e)]
-    regions = np.array(list(zip([chrom]*len(s),s,e,v)), dtype=[('chrom',np.unicode_,8),('start',int),('end',int),('value',float)])
-    return regions
+def moving_average(x, n=5):
+    mask = np.isnan(x)
+    k = np.ones(n, dtype=int)
+    sums = np.convolve(np.where(mask,0,x), k, mode='same')
+    counts = np.convolve(~mask,k,mode='same')
+    return sums/counts
 
 
-def main(args):
-    logging.info(args)
-    mat = scipy.sparse.load_npz(args['input']).astype(float)
-    res = int(args['r'])
-    minD = int(args['d'])
-    insZ = int(args['s'])
-    smthW = int(args['w'])
-    d = minD/res
-    z = insZ/res
-    w = smthW/res
+def smooth_by_MA(x, w, t=1):
+    sm_x = x
+    for i in range(t):
+        sm_x = moving_average(sm_x, w)
+    return sm_x
+
+
+def robust_scale(x, center=True):
+    m = np.nanmedian(x)
+    mad = np.nanmedian(np.abs(x-m))*1.482602
+    if center:
+        y = (x-m)/mad
+    else:
+        y = x/mad
+    return y
+
+
+def rlencode(x, dropNA=False):
+    """
+    Run length encoding.
+    Based on http://stackoverflow.com/a/32681075, which is based on the rle
+    function from R.
+
+    Parameters
+    ----------
+    x : 1D array_like
+        Input array to encode
+    dropNA: bool, optional
+        Drop all runs of NaNs.
+    Returns
+    -------
+    start positions, run lengths, run values
+
+    """
+    where = np.flatnonzero
+    x = np.asarray(x)
+    n = len(x)
+    if n == 0:
+        return (np.array([], dtype=int),
+                np.array([], dtype=int),
+                np.array([], dtype=x.dtype))
+    starts = np.r_[0, where(~np.isclose(x[1:], x[:-1], equal_nan=True)) + 1]
+    lengths = np.diff(np.r_[starts, n])
+    values = x[starts]
+    if dropNA:
+        mask = ~np.isnan(values)
+        starts, lengths, values = starts[mask], lengths[mask], values[mask]
+    return starts, lengths, values
+
+
+def calculate_insulation(mat, z, d):
     N = mat.shape[0]
+    k = np.nansum(mat, axis=1) == 0
+    mat[:,k] = np.nan
+    mat[k,:] = np.nan
     x = np.arange(N)
     I1 = np.maximum(x-d-z, 0)
     I2 = np.maximum(x-d, 0)
     J1 = np.minimum(x+d, N)
     J2 = np.minimum(x+d+z, N)
     insBlocks = [mat[i1:i2,j1:j2] for i1,i2,j1,j2 in zip(I1, I2, J1, J2)]
-    insBlockSums = np.array([np.nansum(blk.toarray()) for blk in insBlocks])
+    insBlockSums = np.array([np.nansum(blk) for blk in insBlocks])
     insBlockSizes = np.array(map(np.product, map(np.shape, insBlocks)))
-    insBlockMeans = insBlockSums / insBlockSizes
-    d1kernel = prepare_derivative_kernel(1)
-    d2kernel = prepare_derivative_kernel(2)
-    smooth_kernel = prepare_smooth_kernel(w, 3)
-    #d1kernel = prepare_derivative_kernel(1, w, 3)
-    #d2kernel = prepare_derivative_kernel(2, w, 3)
-    #concave_regions = find_concave_regions(insBlockMeans, d2kernel)
-    #np.savetxt(sys.stdout, concave_regions, fmt=['%s','%d','%d','%.4f'], delimiter='\t')
-    insBlockSmoothMeans = np.convolve(insBlockMeans, smooth_kernel, mode='same')
+    insBlockNanCounts = np.array(map(np.sum, map(np.isnan, insBlocks)))
+    insBlockMeans = insBlockSums / (insBlockSizes - insBlockNanCounts)
+    return insBlockMeans,insBlockSizes-insBlockNanCounts
 
-    D1 = np.convolve(insBlockMeans, d1kernel, mode='same')
-    D1s = np.convolve(insBlockSmoothMeans, d1kernel, mode='same')
-    D2 = np.convolve(insBlockMeans, d2kernel, mode='same')
-    D2s = np.convolve(insBlockSmoothMeans, d2kernel, mode='same')
-    insTable = pd.DataFrame({'size':insBlockSizes, 'sum':insBlockSums, 'mean':insBlockMeans, 'x':x, 'D1':D1, 'D2':D2, 'D1s':D1s, 'D2s':D2s})
-    insTable.to_csv(sys.stdout, sep='\t', na_rep='NA', header=True, index=False)
+
+def prune_small_rle(rle, minL):
+    indices = rle[0]
+    lengths = rle[1]
+    values = rle[2]
+    N = indices.shape[0]
+    i_left = np.where(lengths<=minL)[0]
+    i_right = i_left + 1
+    i = np.union1d(i_left, i_right)
+    i = i[i<N]
+    k = np.setdiff1d(np.arange(N), i)
+    return indices[k],lengths[k],values[k]
+
+
+def find_local_minimums(x, dx, ddx, minL):
+    N = x.shape[0]
+    dx_rle = prune_small_rle(rlencode(dx>0), minL)
+    ddx_rle = prune_small_rle(rlencode(ddx>0), minL)
+
+    dx0_idx = dx_rle[0]
+    LMIN_idx = dx0_idx[ddx[dx0_idx]>0]
+    LMAX_idx = dx0_idx[ddx[dx0_idx]<0]
+    neighbor_max_dx0_idx = []
+    for idx in LMIN_idx:
+        left_max_indices = LMAX_idx[LMAX_idx<idx]
+        if left_max_indices.shape[0] > 0:
+            left_max_idx = left_max_indices[-1]
+        else:
+            left_max_idx = 0
+        right_max_indices = LMAX_idx[LMAX_idx>idx]
+        if right_max_indices.shape[0] > 0:
+            right_max_idx = right_max_indices[0]
+        else:
+            right_max_idx = N-1
+        neighbor_max_dx0_idx.append([left_max_idx,right_max_idx,idx])
+    x_depth = np.array([(x[i]-x[k],x[j]-x[k]) for i,j,k in neighbor_max_dx0_idx])
+
+    ddx0_idx = ddx_rle[0]
+    neighbor_ddx0_idx = []
+    for idx in LMIN_idx:
+        left_ddx0_indices = ddx0_idx[ddx0_idx<idx]
+        if left_ddx0_indices.shape[0] > 0:
+            left_ddx0_idx = left_ddx0_indices[-1]
+        else:
+            left_ddx0_idx = 0
+        right_ddx0_indices = ddx0_idx[ddx0_idx>idx]
+        if right_ddx0_indices.shape[0] > 0:
+            right_ddx0_idx = right_ddx0_indices[0]
+        else:
+            right_ddx0_idx = N-1
+        neighbor_ddx0_idx.append([left_ddx0_idx,right_ddx0_idx,idx])
+    dx_depth = np.array([(dx[i]-dx[k],dx[j]-dx[k]) for i,j,k in neighbor_ddx0_idx])
+    n = LMIN_idx.shape[0]
+    return pd.DataFrame(np.hstack([LMIN_idx.reshape(n,1),x_depth,dx_depth]),
+            columns=['idx','depth_left','depth_right','slope_left','slope_right'])
+
+
+def main(args):
+    logging.info(args)
+    mat = scipy.sparse.load_npz(args['input']).astype(float).toarray()
+    outPrfx = args['o']
+    res = int(args['r'])
+    insZ = int(args['s'])
+    insD = int(args['d'])
+    min_s = int(args['z'])
+    min_v = float(args['v'])
+    w = int(args['w'])
+    res_boost = 5
+
+    N = mat.shape[0]
+    d = insD/res
+    z = insZ/res
+
+    ins,insN = calculate_insulation(mat, z, d)
+    smoothed_ins = smooth_by_MA(ins, w, 3)
+
+    x = np.arange(N)*res
+    k = np.isnan(smoothed_ins)
+    f = splrep(x[~k], smoothed_ins[~k], k=5)
+    xnew = np.arange(0, N*res, res/res_boost)
+    xnew = xnew[xnew<=np.nanmax(x)]
+    y = robust_scale(splev(xnew, f, der=0, ext=3))
+    dy = robust_scale(splev(xnew, f, der=1, ext=3), center=False)
+    ddy = robust_scale(splev(xnew, f, der=2, ext=3), center=False)
+
+    smoothed_y = smooth_by_MA(y, w, 5)
+    smoothed_dy = smooth_by_MA(dy, w, 5)
+    smoothed_ddy = smooth_by_MA(ddy, w, 10)
+
+    insulations = pd.DataFrame({'x':xnew, 'y':y, 'dy':dy, 'ddy':ddy, 'ys':smoothed_y, 'dys':smoothed_dy, 'ddys':smoothed_ddy})
+    boundaries = find_local_minimums(smoothed_y, smoothed_dy, smoothed_ddy, min_s*res_boost)
+    boundaries['x'] = boundaries.idx*int(res/res_boost) + int(res/res_boost/2)
+
+    insFn = outPrfx + '.insulation.txt'
+    bdFn = outPrfx + '.boundary.txt'
+
+    insulations[['x','ys','dys','ddys']].to_csv(insFn, sep='\t', na_rep='NA', header=True, index=False)
+    boundaries[(boundaries.depth_left>min_v) & (boundaries.depth_right>min_v)].to_csv(bdFn, sep='\t', na_rep='NA', header=True, index=False)
 
 
 if __name__ == '__main__':
